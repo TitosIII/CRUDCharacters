3ac9e25a3ed947662e3a22a63d36d5d102f4f7f2
Iker4000 <aguilar.souza.iker.itzae@gmail.com>
2022-10-10 20:26:02 -0500

    2nd update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from "express"
 import mysql2 from "mysql2"
-import { port } from "./config"
-import {db} from "./mysqldb"
+import { port } from "./config.js"
+import {db} from "./mysqldb.js"
 
+const app = express();
+
+app.listen(port, ()=>{
+    console.log(`Escuchando desde el puerto: ${port}`)
+})
--- src/mysqldb.js
+++ src/mysqldb.js
@@ -14,3 +14,5 @@ export const db = createConnection({
     database:db_name,
     port:db_port
 });
+
+db.connect()
