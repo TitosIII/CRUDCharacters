3e28b4adefffb6b1d33946829c3fdeaf95f0678d
TitosIII <carloscw0304@gmail.com>
2022-10-10 23:59:12 -0500

    test1

--- src/app.js
+++ src/app.js
@@ -5,10 +5,17 @@ import {db} from "./mysqldb.js"
 
 const app = express();
 app.use(bodyParser.json());
+app.set("view engine", "ejs");
 
-app.get("/", ()=>{
-    db.query("select * from characters;")
-})
+app.get("/", (req, res) =>{
+    db.query("select * from characters;", (error, table)=>{
+        if (error){
+            console.log(error)
+        }else{
+            res.render("../public/index.ejs", {table:table})
+        }
+    })
+});
 
 app.listen(port, ()=>{
     console.log(`Escuchando desde el puerto: ${port}`)
