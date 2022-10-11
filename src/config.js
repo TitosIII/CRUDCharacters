f3bc75a9680574915bfb3dd4c0ac28a7bc13726f
TitosIII <carloscw0304@gmail.com>
2022-10-11 00:08:57 -0500

    test2

--- src/config.js
+++ src/config.js
@@ -6,4 +6,4 @@ export const db_password = process.env.DB_PASSWORD || "n0m3l0"
 export const db_name = process.env.DB_NAME || "characters"
 export const db_port = process.env.DB_PORT || 3306
 
-export const dbconfig = process.env.DATABASE_URL || "mysql://root:Titos_304@localhost:3306/characters"
+export const dbconfig = process.env.MYSQL_URL || "mysql://root:Titos_304@localhost:3306/characters"
